07de93a57eec1f2ac2248b83748a6bb53b175882
progvan <vanatom22@gmail.com>
2025-03-13 14:53:21 +0100

    add signup func

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,10 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
   connectDB();
