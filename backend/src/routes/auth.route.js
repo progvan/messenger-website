06260adf24b89291f602bf4150307275d205a9ac
progvan <vanatom22@gmail.com>
2025-03-15 00:37:12 +0100

    validation to signup, login and add update profile route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,9 +4,9 @@ import { login, logout, signup } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
 
 export default router;
