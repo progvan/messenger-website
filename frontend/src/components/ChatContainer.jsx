542447138b77bfb97cec553f55e79568868edc46
progvan <vanatom22@gmail.com>
2025-04-16 14:44:05 +0200

    add chat header

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import ChatHeader from './ChatHeader'
+import MessageInput from './MessageInput'
 
 const ChatSelected = () => {
   return (
-    <div>ChatSelected</div>
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+
+      <p>loading...</p>
+
+      <MessageInput />
+    </div>
   )
 }
 
