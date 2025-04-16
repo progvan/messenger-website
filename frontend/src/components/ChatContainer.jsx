e2ec5d1c2d4cd503bd3633355a7eecb8f4af76bb
progvan <vanatom22@gmail.com>
2025-04-16 15:24:10 +0200

    add message skeleton while messages is loading in chat

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ChatHeader from './ChatHeader'
 import MessageInput from './MessageInput'
+import MessageSkeleton from './skeletons/MessageSkeleton'
 
 const ChatSelected = () => {
+
+  if(true)
   return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
 
-      <p>loading...</p>
+      <MessageSkeleton />
 
       <MessageInput />
     </div>
