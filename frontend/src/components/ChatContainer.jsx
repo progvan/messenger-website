6bb1db9eb9f4cea2e2a01ab07bf794b5f44824b9
progvan <vanatom22@gmail.com>
2025-04-17 17:14:56 +0200

    fix receiverId, messages save in db

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -5,13 +5,10 @@ import MessageSkeleton from './skeletons/MessageSkeleton'
 
 const ChatSelected = () => {
 
-  if(true)
   return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
-
-      <MessageSkeleton />
-
+      <p>messages</p>
       <MessageInput />
     </div>
   )
